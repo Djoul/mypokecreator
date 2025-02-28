92fb935d0c21c93101c33169001e3d8d3e59de36
Julien Legrand <julien@nomala.com>
2025-02-28 10:50:15 +0100

    Improve for google search

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -126,7 +126,7 @@ const Index = () => {
       </div>
       
       <footer className="absolute bottom-4 left-0 right-0 text-center text-sm text-gray-500">
-        Pokémon is © 1995-2025 Nintendo. Made with <svg class="w-4 h-4 inline text-red-500" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg> by <a href="https://x.com/JulienLeg78" class="text-gray-600 hover:text-gray-800">@JulienLeg78</a>
+        Pokémon is © 1995-2025 Nintendo. <a href="https://github.com/Djoul/mypokecreator" className="text-gray-600 hover:text-gray-800">Github</a> | Made with <svg className="w-4 h-4 inline text-red-500" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg> by <a href="https://x.com/JulienLeg78" className="text-gray-600 hover:text-gray-800">@JulienLeg78</a>
       </footer>
     </div>
   );
